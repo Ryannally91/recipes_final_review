ac20c4fb7cf85b8845f3e7b0ba3cd53c452cc1cb
Ryan Nally <79378346+Ryannally91@users.noreply.github.com>
2022-04-28 13:47:43 -0500

    Update server.py

--- server.py
+++ server.py
@@ -1,7 +1,6 @@
 from flask_app import app
 from flask_app.controllers import users
- #, recipes needed?
-#Remember to import all CONTROLLERS!!!
+ 
 
 if __name__ == "__main__":
     app.run(debug=True)
